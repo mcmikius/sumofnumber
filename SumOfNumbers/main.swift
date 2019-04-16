748dde7e4a02f82d6e905617efa0119b00c3ff6b
Michail Bondarenko <mcmikius15@gmail.com>
2019-04-16 14:38:17 +0300

    Initial Commit

---
+++ SumOfNumbers/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SumOfNumbers
+//
+//  Created by Michail Bondarenko on 4/16/19.
+//  Copyright © 2019 Michail Bondarenko. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
